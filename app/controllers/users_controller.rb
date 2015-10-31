a02516e244f062148e03e1878e702d37117f282c
jialiyang <jialiyang1107@gmail.com>
2015-10-31 15:22:49 +0000

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
                                   :password, :password_confirmation)
         @user = User.new(secure_params)
         if @user.save
-        	flash[:success] = "Welcome to the Twitter App!" 
-           redirect_to @user# Handle a successful save.
+        	remember @user       
+          flash[:success] = "Welcome to the Sample App!"   
+          redirect_to @user# Handle a successful save.
         else
             render 'new'# Handle an unsuccessful save.     
         end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   
+  
+
           resources :users 
           root 'static_pages#home'
 
@@ -7,6 +9,9 @@ Rails.application.routes.draw do
           get 'about'   => 'static_pages#about'
           get 'contact' => 'static_pages#contact'
           get 'signup'  => 'users#new'
+          get    'login'   => 'sessions#new'
+          post   'login'   => 'sessions#create'
+          delete 'logout'  => 'sessions#destroy'
 
       end
 
